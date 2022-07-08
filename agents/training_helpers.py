86f4c23d9cc89938210ac23668dbe44652f2d341
Kamil Chrustowski <chrustek123456789@gmail.com>
2022-07-08 12:16:57 +0200

    Extracted Worker to the outer module

--- agents/training_helpers.py
+++ agents/training_helpers.py
@@ -1,6 +1,13 @@
-from typing import Dict, List, Type, Callable
+from typing import Any, Dict, List, Optional, Tuple, Type, Callable
 import torch as t
 from torch import nn, optim
+from torch.distributions import Categorical
+
+import numpy as np
+
+from . import Agent
+
+from .utils import HeartsStateParser, Memory, Trajectory
 
 class Optimizers:
     _optimizers: Dict[str, Type[optim.Optimizer]] = {
@@ -56,3 +63,71 @@ def build_model(layers: List[int], activations: List[str], initializer: str, ini
             Initializers.get('const')(module.bias, val=0)
 
     return qnet
+
+
+class Worker(nn.Module, Agent):
+    def __init__(self, policy: nn.Module, critic: nn.Module, action_size, parser: HeartsStateParser, callback):
+        nn.Module.__init__(self)
+        Agent.__init__(self, False, 0.0, 0.0, 0.0, None)
+        self.policy = policy
+        self.critic = critic
+        self.rollouts = Memory[Trajectory](None, Trajectory)
+        self.parser = parser
+        self.callback = callback
+        self.training = True
+        self.action_size = action_size
+        self.last_prob = 0.0
+        self.last_val = 0.0
+        self.learning_device = "cuda" if t.cuda.is_available() else 'cpu'
+        
+    def get_action(self, state: Any, invalid_actions: Optional[List[int]] =None): 
+        logits: t.Tensor
+        state: t.Tensor =self.parser.parse(state)
+        possible_actions = possible_actions = set(range(0, self.action_size))
+  
+        if invalid_actions: possible_actions -= set(invalid_actions)
+        possible_actions = list(possible_actions)
+        self.last_possible_actions = possible_actions
+        with t.no_grad():
+            self.eval()
+            logits, value = self(state)
+            logits = logits.cpu().squeeze(0)
+            probs: t.Tensor = t.softmax(logits, dim=0)	
+        
+        self.train()
+        possible_actions = list(possible_actions)
+        probs_gathered: np.ndarray = probs.gather(0, t.as_tensor((possible_actions))) +1e-8
+        probs_gathered = probs_gathered/probs_gathered.sum()
+        dist = Categorical(probs_gathered)
+        idx = dist.sample().item()
+        action = possible_actions[idx]
+        self.last_prob = probs_gathered[idx].item()
+        self.last_val = value.item()
+        return action
+    
+    def get_best_action(self, state, invalid_actions: Optional[List[int]] =None): return self.get_action(state, invalid_actions)
+    
+    @property 
+    def algorithm_name(self) -> str:
+        return 'PPO'
+    
+    def get_name(self) -> str:
+        return super().get_name() + " - PPO-worker"
+    
+    def remember(self, state, action, reward):
+        if not isinstance(state, t.Tensor): state= self.parser.parse(state)
+        #Function adds information to the memory about last action and its results
+        self.rollouts.store(Trajectory(state, action, reward, self.last_prob, value=self.last_val))
+    
+    def set_temp_reward(self, discarded_cards: dict, point_deltas: dict):	
+        super().set_temp_reward(discarded_cards, point_deltas)
+        if not self.training: return
+  
+        self.remember(self.parser.parse(self.previous_state), self.previous_action, -self.current_reward)
+
+    def set_final_reward(self, points: dict):
+        super().set_final_reward(points)
+        self.callback(self)
+        
+    def forward(self, state:t.Tensor) -> Tuple[t.Tensor, t.Tensor]:
+        return self.policy(state.to(self.learning_device)), self.critic(state.to(self.learning_device))  
