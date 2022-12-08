edbe209c5a9c13eba259f7667655f9a53ab9e15b
Kamil Chrustowski <kachr@duck.com>
2022-12-08 09:47:42 +0100

    random agent implementation

--- agents/__init__.py
+++ agents/__init__.py
@@ -7,7 +7,7 @@ from numpy.random._generator import default_rng, Generator
 from .utils import small_deck, full_deck
 import numpy as np
 from collections import deque
-
+import random
 
 INVALID_ACTION_PENALTY = 50
 VICTORY_PENALTY = -100
@@ -99,6 +99,34 @@ class Agent(Player, ABC):
         self.previous_action = action
         return small_deck[action] if not self.full_deck else full_deck[action]
 
+
+class RandomPlayer(Player):
+    """
+    Makes random moves (but according to the rules)
+    """
+    instances = 1
+    def __init__(self):
+        self.number = RandomPlayer.instances
+        RandomPlayer.instances += 1
+
+    def make_move(self, game_state: dict, was_previous_move_wrong: bool) -> Card:
+        if not game_state["discard"]:
+            return random.choice(game_state["hand"])
+        else:
+            options = list(filter(lambda card: card.suit == list(game_state["discard"])[0].suit, game_state["hand"]))
+            if len(options) > 0:
+                return random.choice(options)
+            else:
+                return random.choice(game_state["hand"])
+
+    def get_name(self):
+        return f"RandomPlayer{self.number}"
+
+    def set_temp_reward(self, discarded_cards: dict, point_deltas: dict):
+        pass
+
+    def set_final_reward(self, points: dict):
+        pass
 
 from .pg import REINFORCEAgent
 from .ac import ACAgent
